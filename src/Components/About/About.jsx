093f3d8e66d812f4337fa79b25e443b54fc52fd9
vaishalikamble47 <vaishalikamble4799@gmail.com>
2024-01-25 21:19:35 +0530

    fixed style bugs

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
             <h2 className='a-heading'>About Me</h2>
             <div className="about ">
 
-            <p className="a-para" style={{textAlign:"justify"}}>
+            <p className="" style={{textAlign:"justify"}}>
                     Enthusiastic and dedicated React.Js Web Developer with a passion
-                    for crafting engaging and user centric Web applications. A recent graduate in Solid faundation in web Development technologies, including ReactJS, React Router,Redux and Redux toolkit,Javascript,CSS3,Bootstrap,SASS and HTML5.Eager to contribute to innovative projects, learn, and grow within a collaborative development environment.<br />
+                    for crafting engaging and user centric Web applications. A recent graduate in Solid faundation in web Development technologies, including ReactJS, React Router,Redux and Redux toolkit,Javascript,CSS3,Bootstrap,SASS and HTML5.Eager to contribute to innovative projects, learn, and grow within a collaborative development environment.
                     I'm actively seeking opportunities to kick-start my career as React.Js Web Developer. Open to collaboretions, and connecting with fellow developers.Let's build something amazing together!
                     <br />
 
